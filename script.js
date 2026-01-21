2e59bdad8a891e914ca082a22282a8d2f6529e63
jickzx <kingkongjay1@gmail.com>
2026-01-21 12:22:04 +0000

    testing to add more images

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ const projectData = {
     media: [
       {
         type: "image",
-        src: "images/POP.png"
+        src: "images/POP.png", src: "images/hacknotts25.png", src: "images/P-O-P.png"
       },
       {
         type: "video",
