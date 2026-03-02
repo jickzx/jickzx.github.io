bbeecbc680c5936274dc433cc77195085f2fb822
jickzx <kingkongjay1@gmail.com>
2026-03-02 08:35:10 +0000

    next.js and vercel added to hide api key

---
+++ grid/cesium-next/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+        permanent: false,
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
